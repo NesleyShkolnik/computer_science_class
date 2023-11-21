9c6745bde9219d32010c4fdecc370c051df363d5
NesleyShkolnik <144588302+NesleyShkolnik@users.noreply.github.com>
2023-11-21 15:25:23 +0200

    Update lesson_2.java

--- lesson_2.java
+++ lesson_2.java
@@ -1,1 +1,64 @@
-cippikf
+1
+class Main {
+  public static void main(String[] args) {
+    int x=1,y=2,z=3;
+    System.out.println(x+"\n"+y+"\n"+z+"\n");
+  }
+}
+
+2
+class Main {
+  public static void main(String[] args) {
+    doule x=1.5;
+    int y=4;
+    System.out.println(x+"   "+y);
+  }
+}
+
+3
+class Main {
+  public static void main(String[] args) {
+    char a='N',b='E',c='S',d='L',h='Y';
+    System.out.println(a+""+b+c+d+b+h);
+  }
+}
+
+4
+class Main {
+  public static void main(String[] args) {
+    int x=4, y=5;
+    System.out.println(x+" is less than "+y);
+  }
+}
+
+5
+class Main {
+  public static void main(String[] args) {
+    int x=3, y=8, z=5;
+    System.out.println("The average number is "+((x+y+z)/3));
+  }
+}
+ c vvc 
+6
+class Main {
+  public static void main(String[] args) {
+    int rib_a=7, rib_b=4;
+    System.out.println("Rib c to the power of 2 is "+((rib_a*rib_a)+(rib_b*rib_b)));
+  }
+}
+7
+class Main{
+  public static void main(String[] args){
+    int x=6, y;
+    System.out.println("y is "+(y=(2*x)+5));
+  }
+}
+
+8
+class Main{
+  public static void main(String[] args) {
+    String part1="I like to study computer science",part2="and therefore ready to invest",part3=" hours a day for practice";
+    int hours=4;
+    System.out.println(part1+" "+part2+" "+hours+part3);
+  }
+}
