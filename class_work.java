13ac4064dcd8ba66de306290c78b8476c1a93965
NesleyShkolnik <144588302+NesleyShkolnik@users.noreply.github.com>
2023-12-21 08:03:48 +0200

    Update class_work.java

--- class_work.java
+++ class_work.java
@@ -35,5 +35,56 @@ public class Main {
           {
               System.out.println("Error");
           }
+        //תרגילי כיתה
+		//1
+		System.out.println("Enter a 5 digit number: ");
+		int num = scan.nextInt();
+		int units = num % 10;
+	    int tens = num / 10 % 10;
+	    int hundreds = num / 100 % 10;
+	    int thousands = num / 1000 % 10;
+	    int tens_thousands = num / 10000;
+	    if (units == tens_thousands)
+	    {
+	    	if (tens == thousands)
+	    	{
+	    		System.out.println("The nuber is palindrome");
+	    	}
+	    }
+	    else
+	    {
+	    	System.out.println("The nuber is not palindrome");
+	    }
+	    //2
+	    System.out.println("Enter a 4 digit number: ");
+		int num2 = scan.nextInt();
+		int units2 = num2 % 10;
+	    int tens2 = num2 / 10 % 10;
+	    int hundreds2 = num2 / 100 ;
+	    int thousands2 = num2 / 1000 ;
+	    int sum = units2 + tens2 + hundreds2 + thousands2;
+	    System.out.println("The sum of the 4 digits is: " + sum);
+	    //3
+	    System.out.println("Enter a 6 digit number");
+		int num = scan.nextInt();
+		int units = num % 10;
+	    int tens = num / 10 % 10;
+	    int hundreds = num / 100 % 10;
+	    int thousands = num / 1000 % 10;
+	    int hundreds_thousands =  num / 100000;
+	    int evensSum = units + hundreds + hundreds_thousands;
+	    int unevensSum = tens + thousands;
+	    if (evensSum > unevensSum)
+	    {
+	    	System.out.println("The even digit sum are bigger: " + evensSum + ">" + unevensSum);
+	    }
+	    if (unevensSum > evensSum)
+	    {
+	    	System.out.println("The uneven digit sum are bigger: " + unevensSum + ">" + evensSum);
+	    }
+	    else
+	    {
+	    	System.out.println("The digit sums are equals: " + evensSum + "=" +unevensSum);
+	    }
     }    
 }
