4802bd6b3e3988ea83d1dadab81d3b3be2f065ad
NesleyShkolnik <144588302+NesleyShkolnik@users.noreply.github.com>
2024-01-16 17:37:13 +0200

    Update lesson5.java

--- lesson5.java
+++ lesson5.java
@@ -1,256 +1,155 @@
 import java.util.Scanner;
-public class lesson5
-{
-	//1
-    public static void printingName()
-    {
-        Scanner scan = new Scanner(System.in);
+import java.lang.Math;
+
+public class Main {
+    private static Scanner scan = new Scanner(System.in);
+
+    // 1
+    public static void printingName() {
         System.out.println("Enter your name: ");
         String name = scan.next();
         System.out.println("Hello " + name + "!");
     }
-    public static void main(String[] args)
-    {
+
+    // 2
+    public static void div5(int num) {
+        num = num / 5;
+        System.out.println("The number divided by 5 is " + num);
+    }
+
+    // 3
+    public static int mul(int num1, int num2) {
+        int mul = num1 * num2;
+        return mul;
+    }
+
+    // 4
+    public static void sumAndAverage(int num1, int num2, int num3) {
+        int sum = num1 + num2 + num3;
+        int average = sum / 3;
+        System.out.println("The sum is: " + sum);
+        System.out.println("The average is: " + average);
+    }
+
+    // 5
+    public static void sumAndAverage(int radius) {
+        double pi = 3.14;
+        double Perimeter = 2 * pi * radius;
+        double area = pi * Math.pow(radius, 2);
+        System.out.println("The Perimeter is: " + Perimeter);
+        System.out.println("The area is: " + area);
+    }
+
+    // 6
+    public static int calculateRibC(int rib_a, int rib_b) {
+        int rib_c_squared = rib_a * rib_a + rib_b * rib_b;
+        int rib_c = (int) Math.sqrt(rib_c_squared);
+        return rib_c;
+    }
+
+    // 7
+    public static void mixletters(char letter1, char letter2, char letter3) {
+        System.out.println("The mixed letter is: " + letter3 + letter2 + letter1);
+    }
+
+    // 8
+    public static int invoicSeries(int firstNum, int difference, int memberNum) {
+        int num = firstNum + (memberNum - 1) * difference;
+        return num;
+    }
+
+    // 9
+    public static int strPow(String str) {
+        int size = str.length();
+        int num = (int) Math.pow(size, 2);
+        return num;
+    }
+
+    // 10
+    public static void twoStr(String str1, String str2) {
+        int size1 = str1.length();
+        int size2 = str2.length();
+        if (size1 > size2) {
+            System.out.println(str1 + " is bigger");
+        }
+	else {
+            System.out.println(str2 + " is bigger");
+        }
+    }
+
+    // 11
+    public static void strCheck(String str, int num) {
+        int size = str.length();
+        if (size == num) {
+            System.out.println(true);
+        } 
+	else {
+            System.out.println(false);
+        }
+    }
+
+    // 12
+    public static void equal(int num1, int num2) {
+        if (num1 == num2) {
+            System.out.println("equals");
+        } 
+	else {
+            System.out.println("not equals");
+        }
+    }
+
+    // 13
+    public static void checks(String str, char char1) {
+        System.out.println("Enter a 4 words string:");
+        str = scan.nextLine();
+        System.out.println("Enter one char:");
+        char1 = scan.next().charAt(0);
+
+        if (str.contains(String.valueOf(char1))) {
+            System.out.println("true");
+        }
+	else {
+            System.out.println("false");
+        }
+    }
+
+    public static void main(String[] args) {
         printingName();
-    }
-    //2
-    public class Main 
-    {
-        public static void div5(int num)
-        {
-            num = num / 5;
-            System.out.println("The number divided by 5 is " + num);
-        }
-
-        public static void main(String[] args)
-        {
-            int num = 24;
-            div5(num);
-        }
-    }
-    //3
-    public class Main 
-    {
-        public static int mul(int num1, int num2)
-        {
-            int mul = num1 * num2;
-            return mul;
-        }
-
-        public static void main(String[] args)
-        {
-            int num1 = 24;
-            int num2 = 3;
-            mul(num1, num2);
-            System.out.println("The product of the 2 number is: " + mul);
-        }
-    }
-    //4
-    public class Main 
-    {
-        public static void sumAndAverage(int num1, int num2, int num3)
-        {
-            int sum = num1 + num2 + num3;
-            int average = sum / 3;
-            System.out.println("The sum is: " + sum);
-            System.out.println("The average is: " + average);
-        }
-
-        public static void main(String[] args)
-        {
-            int num1 = 24;
-            int num2 = 3;
-            int num3  = 17;
-            sumAndAverage(num1, num2, num3);
-        }
-    }
-    //5
-    import java.lang.Math;
-    public class Main 
-    {
-        public static void sumAndAverage(int radius)
-        {
-            double pi = 3.14;
-            double Perimeter = 2  * pi * radius;
-            double area = pi * Math.pow(radius, 2);
-            System.out.println("The Perimeter is: " + Perimeter);
-            System.out.println("The area is: " + area);
-        }
-
-        public static void main(String[] args)
-        {
-            int radius  = 17;
-            sumAndAverage(radius);
-        }
-    }
-    //6
-    public class Main 
-    {
-        public static int calculateRibC(int rib_a, int rib_b)
-        {
-            int rib_c_squared = rib_a * rib_a + rib_b * rib_b;
-            int rib_c = (int) Math.sqrt(rib_c_squared);
-            return rib_c;
-        }
-
-        public static void main(String[] args)
-        {
-            int rib_a = 17;
-            int rib_b = 6;
-            int rib_c = calculateRibC(rib_a, rib_b);
-            System.out.println("The rib c is " + rib_c);
-        }
-    }
-    //7
-    public class Main 
-    {
-        public static void mixletters(char letter1, char letter2, char letter3)
-        {
-            System.out.println("The mixed letter is: " + letter3 + letter2 + letter1);
-        }
-
-        public static void main(String[] args)
-        {
-            char letter1 = 'n';
-            char letter2 = 'a';
-            char letter3 = 'd';
-            mixletters(letter1, letter2, letter3);
-        }
-    }
-    //8
-    public class Main 
-    {
-        public static int invoicSeries(int firstNum, int difference, int memberNum)
-        {
-            int num = firstNum + (memberNum - 1) * difference
-            return num;
-        }
-        public static void main(String[] args)
-        {
-            int firstNum = 4;
-            int difference = 2;
-            int memberNum = 2;
-            invoicSeries(firstNum, difference, memberNum);
-            System.out.println("The number is: " + num);
-        }
-    }
-}
- //9
-    public class Main {
-        public static int strPow(String str)
-        {
-            int size = str.length();
-            int num = (int) Math.pow(size, 2);
-            return num;
-        }
-
-        public static void main(String[] args)
-        {
-            String str = new String("nesley");
-            int result = strPow(str);
-            System.out.println("The power of the number of characters in the string is: " + result);
-        }
-    }
-    //10
-    public class Main
-    {
-        public static int twoStr(String str1, String str2)
-        {
-            int size1 = str1.length();
-            int size2 = str2.length();
-            if (size1 > size2)
-            {
-                System.out.println(str1 + " is bigger");
-            }
-            else
-            {
-                System.out.println(str2 + " is bigger");
-            }
-            return 0;
-        }
-
-        public static void main(String[] args)
-        {
-            String str1 = new String("nesley");
-            String str2 = new String("dana");
-            twoStr (str1, str2);
-        }
-    }
-    //11
-    public class Main
-    {
-        public static int strCheck(String str, int num)
-        {
-            int size = str.length();
-            if (size == num)
-            {
-                System.out.println(true);
-            }
-            else
-            {
-                System.out.println(false);
-            }
-            return 0;
-        }
-
-        public static void main(String[] args)
-        {
-            String str = new String("nesley");
-            int num = 8;
-            strCheck (str, num);
-        }
-    }
-    //12
-    public class Main
-    {
-    	public static void main(String[] args) 
-    	{
-
-    		Random r=new Random();
-    	    int num1=r.nextInt(6)+1;
-    	    int num2=r.nextInt(6)+1;
-    		equal(num1,num2);
-    	}
-    	
-    	public static void equal(int num1,int num2)
-    	{
-    	    if (num1==num2)
-    	    {
-    	        System.out.println("eqauals");
-    	    }
-    	    else
-    	    {
-    	         System.out.println("not eqauals");
-    	        
-    	    }
-    	}    
-    }
-    //13
-    public class Main
-    {
-        private static Scanner scan = new Scanner(System.in);
-
-        public static void main(String[] args) {
-            char char1 = ' ';
-            String str = " ";
-            checks(str, char1);
-        }
-
-        public static void checks(String str, char char1)
-        {
-            System.out.println("Enter a 4 words string:");
-            str = scan.nextLine();
-            System.out.println("Enter one char:");
-            char1 = scan.next().charAt(0);
-
-            if (str.contains(String.valueOf(char1)))
-            {
-                System.out.println("true");
-            }
-            else
-            {
-                System.out.println("false");
-            }
-        }
+        int num = 24;
+        div5(num);
+        int num1 = 24;
+        int num2 = 3;
+        int mulResult = mul(num1, num2);
+        System.out.println("The product of the 2 numbers is: " + mulResult);
+        int num3 = 17;
+        sumAndAverage(num1, num2, num3);
+        int radius = 17;
+        sumAndAverage(radius);
+        int rib_a = 17;
+        int rib_b = 6;
+        int rib_c = calculateRibC(rib_a, rib_b);
+        System.out.println("The rib c is " + rib_c);
+        char letter1 = 'n';
+        char letter2 = 'a';
+        char letter3 = 'd';
+        mixletters(letter1, letter2, letter3);
+        int firstNum = 4;
+        int difference = 2;
+        int memberNum = 2;
+        int invoicResult = invoicSeries(firstNum, difference, memberNum);
+        System.out.println("The number is: " + invoicResult);
+        String str = new String("nesley");
+        int result = strPow(str);
+        System.out.println("The power of the number of characters in the string is: " + result);
+        String str1 = new String("nesley");
+        String str2 = new String("dana");
+        twoStr(str1, str2);
+        String str3 = new String("nesley");
+        int num4 = 8;
+        strCheck(str3, num4);
+        equal(5, 5);
+        char char1 = ' ';
+        String str4 = " ";
+        checks(str4, char1);
     }
 }
