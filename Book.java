eb9e94cdc3f312ff9e196074ce4427f633fee0a5
NesleyShkolnik <144588302+NesleyShkolnik@users.noreply.github.com>
2024-04-29 00:52:35 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,3 +1,4 @@
+class Book
 {
     String name;
     String author;
@@ -8,8 +9,49 @@
         author=authorName;
         categoryNumber=num;
     }
+    public String getName()
+    {
+        return name;
+    }
+    public String getAuthor()
+    {
+        return author;
+    }
+    public int getCategoryNumber()
+    {
+        return categoryNumber;
+    }
+    public void setName(String bookName)
+    {
+        name = bookName;
+    }
+    public void setAuthor(String bookAuthor)
+    {
+        author = bookAuthor;
+    }
+    public void setCategoryNumber(int num)
+    {
+        categoryNumber = num;
+    }
+    public Book(Book other)
+    {
+        name = other.name;
+        author = other.author;
+        categoryNumber = other.categoryNumber;
+    }
     public String toString()
     {
         return "The book named " + name + "was written by " + author + "and is in the category number" + categoryNumber;
+    }
+    public boolean check(Book otherBook)
+    {
+        if(categoryNumber == otherBook.categoryNumber)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
